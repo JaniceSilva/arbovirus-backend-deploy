842f6a0c28d7587bc06c6bd98e8c5a8ff9a23a98
JaniceGonzagacs <janicegonzagacs@gmail.com>
2025-07-24 11:17:31 -0300

    Sua mensagem de commit aqui

--- application.py
+++ application.py
@@ -5,14 +5,12 @@ Arquivo principal para AWS Elastic Beanstalk
 import os
 import sys
 
-# Adicionar diretório src ao path
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
 
-from main import app  # import correto
+from src.main import app
 
-# Elastic Beanstalk espera uma variável chamada 'application'
 application = app
- 
+
 if __name__ == '__main__':
-    # Para desenvolvimento local
-    application.run(host='0.0.0.0', port=5000, debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    application.run(host='0.0.0.0', port=port, debug=False)
